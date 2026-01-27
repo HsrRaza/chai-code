42436660e70ce9b910a750239d561976c6ad3807
Nabil <nabil9341@gmail.com>
2026-01-27 22:36:00 +0530

    fix/navbar

---
+++ chai-code/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-auto ">
+
+        <div className="md:w-[65%] mx-auto flex justify-between items-center p-2 shadow-xl shadow-blue-200 rounded-xl  sticky ">
+
+        {/* logo */}
+        <div>
+            <h2 className="md:text-4xl">ChaiCode</h2>
+        </div>
+        <div className="flex gap-6">
+            <h3 className="md:text-xl">Cohorts</h3>
+            <h3 className="md:text-xl">Udemy</h3>
+            <h3 className="md:text-xl">Docs</h3>
+            <h3 className="md:text-xl">Reviews</h3>
+        </div>
+
+        {/* btn login */}
+        <div>
+            <button className="px-5 py-2 bg-blue-500 md:text-2xl rounded-xl text-center  outline-none text-stone-100">Login</button>
+        </div>
+        
+        {/* <div className="absolute w-24 h-24  bg-linear-to-b from-blue-400 to-blue-400"></div> */}
+        </div>
+    </div>
+
+  )
+}
+
+export default Navbar
