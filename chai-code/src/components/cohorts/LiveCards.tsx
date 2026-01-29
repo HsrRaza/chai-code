dc6121765445dd1c56f8b83a76d9fc6972f68e09
HsrRaza <hr.raza04@gmail.com>
2026-01-29 22:08:04 +0530

    fix/grid

--- chai-code/src/components/cohorts/LiveCards.tsx
+++ chai-code/src/components/cohorts/LiveCards.tsx
@@ -1,32 +1,42 @@
-
 export const LiveCards = () => {
   return (
-    <div>
-        <Cards/>
+    <div className=" grid grid-cols-2 md:grid-cols-3  ">
+      <Cards />
+      <Cards />
+      <Cards />
+      
     </div>
-  )
-}
+  );
+};
 
-export default LiveCards
+export default LiveCards;
 
-const Cards = ()=>{
-return (
-    <div>
-        <div>
-            Yt Video
+const Cards = () => {
+  return (
+    <div className="  flex  md:items-center   md:justify-center">
+      <div className="w-60 sm:w-100 rounded-xl p-4  shadow-xl">
+        <div className="w-full h-40 sm:h-60 bg-black rounded-xl"></div>
+        <div className="p-2">
+          <h3 className="sm:text-3xl">Web dev Cohort-Live 2.0</h3>
+          <p className="sm:text-md">
+            Learn to build software for web with best and <br /> latest tech
+            stack
+          </p>
         </div>
-        <div>
-            <h3>Web dev Cohort-Live 1.0</h3>
-            <p>Learn to build software for web with best and <br /> latest tech  stack</p>
+        <div className="flex gap-4 items-center  p-2">
+          <div className="p-2 border rounded-xl">
+            <span className="sm:text-2xl">6999 INR</span>
+          </div>
+          <div>
+            <span className="line-through sm:text-lg">8999 INR</span>
+          </div>
         </div>
-        <div>
-            <div>6999 INR</div>
-            <div>8999 INR</div>
+        <div className="p-2 w-full">
+          <button className="w-full sm:px-2 sm:py-3 bg-blue-500 rounded-xl sm:text-xl outline-none text-stone-100">
+            Buy Now
+          </button>
         </div>
-        <div>
-            <button>Buy Now </button>
-        </div>
-            
+      </div>
     </div>
-)
-}
+  );
+};
--- chai-code/src/components/cohorts/Text.tsx
+++ chai-code/src/components/cohorts/Text.tsx
@@ -1,9 +1,9 @@
 
 const Text = () => {
   return (
-    <div className="text-center p-4 ">
-        <h1 className="text-7xl text-shadow-stone-500 m-2">Cohorts</h1>
-        <h3 className="text-4xl/relaxed m-4">Live training Classes </h3>
+    <div className=" p-4 ml-2">
+        <h1 className="text-5xl text-shadow-stone-500 m-2">Our Cohorts</h1>
+        <h3 className="text-2xl/relaxed m-4">Learn in engaging <span className="text-amber-800"> live classes</span> , collaborate with peers, share ideas, and grow together as part of a vibrant learning community. </h3>
     </div>
   )
 }
